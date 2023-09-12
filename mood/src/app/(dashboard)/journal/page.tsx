8a518be295ab999abb29e114678966f00be4a3e8
ahmadawji <ahmadawjics@gmail.com>
2023-09-12 22:20:15 +0300

    1. Write function to get user using his clerk id 2.  Write funcion to fetch user's journal's from the DB inside the JournalPage component

--- mood/src/app/(dashboard)/journal/page.tsx
+++ mood/src/app/(dashboard)/journal/page.tsx
@@ -1,3 +1,19 @@
-export default function JournalPage() {
+import { getUserByClerkId } from "../../../../utils/auth"
+import { prisma } from "../../../../utils/db"
+
+const getEntries = async () => {
+  const user = await getUserByClerkId()
+  const entries = await prisma.journalEntry.findMany({
+    where: { userId: user.id },
+    orderBy: { createdAt: "desc" },
+  })
+
+  return entries
+}
+
+export default async function JournalPage() {
+  const entries = await getEntries()
+  console.log("🚀 ~ file: page.tsx:16 ~ JournalPage ~ entries:", entries)
+
   return <div>JournalPage</div>
 }
