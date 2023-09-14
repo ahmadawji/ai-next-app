0a13955fba01a9ce72ddb52ec00bb878661df3d2
ahmadawji <ahmadawjics@gmail.com>
2023-09-15 00:32:46 +0300

    Add react autosave feature

--- mood/src/components/Editor.tsx
+++ mood/src/components/Editor.tsx
@@ -1,19 +1,37 @@
 "use client"
 import { JournalEntry } from "@prisma/client"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { updateEntry } from "../../utils/api"
+import { useAutosave } from "react-autosave"
 
 const Editor = ({ entry }: { entry: JournalEntry | null }) => {
   const [value, setValue] = useState(entry?.content)
+  const [isLoading, setIsLoading] = useState(false)
+  useAutosave({
+    data: value,
+    onSave: async (_value) => {
+      setIsLoading(true)
+      await updateEntry(entry?.id as string, _value as string)
+      setIsLoading(false)
+    },
+  })
+
   return (
     <div className="w-full h-full">
+      <div className="relative flex h-3 w-3 m-2 float-right">
+        {isLoading && (
+          <>
+            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+            <span className="relative inline-flex rounded-full h-3 w-3 bg-green-400"></span>
+          </>
+        )}
+      </div>
+
       <textarea
         value={value}
         className="w-full h-full p-8 text-xl outline-none"
         onChange={(e) => {
-          updateEntry(entry?.id as string, e.target.value).then(({ data }) => {
-            setValue(data.content)
-          })
+          setValue(e.target.value)
         }}
       />
     </div>
